218371b225612ec0bc0608873affcc20ab2af645
Karl Solgård <karl.f91@gmail.com>
2014-02-10 18:18:04 +0100

    hide menu on scroll + responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,6 +18,31 @@ $(document).ready(function(){
 			    columnWidth : 442		
             });
         }, 1000);
-        
+    });
+
+    var myval = 0;
+    var lastScrollTop = 0, delta = 5;
+    var holdsettimeout2;
+    $(window).scroll(function(event){
+        clearTimeout(holdsettimeout2);
+        holdsettimeout2 = setTimeout(function () {
+           var st = $(this).scrollTop();
+       
+           if(Math.abs(lastScrollTop - st) <= delta)
+              return;
+       
+           if (st > lastScrollTop){
+                $('.navbar').animate({
+                'margin-top': '-55px'
+            }, 500);
+             console.log("down");
+           } else {
+                $('.navbar').animate({
+                'margin-top': '0'
+            }, 500);
+             console.log("up");
+           }
+           lastScrollTop = st;
+       }, 500);
     });
 });
